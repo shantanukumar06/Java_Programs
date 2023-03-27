e1c5aa43cb218689877aed936e8b1cf3d08b600c
Shantanu Kumar <128491812+shantanukumar06@users.noreply.github.com>
2023-03-27 21:08:28 +0530

    Add files via upload

---
+++ Circlearea.java
@@ -0,0 +1,8 @@
+class Circlearea 
+ {  
+   public static void main (String[] x)  
+    {   double r=7, pi=3.14 ,area;  //local variable
+        area=pi*r*r;     
+        System.out.println("Area of circle is : "+area);  
+    }
+}  

cccaf216aafe80a76b0683de860ef8ea1801a9e4
Shantanu Kumar <128491812+shantanukumar06@users.noreply.github.com>
2023-03-27 20:32:40 +0530

    Add files via upload

---
+++ localprewith_swap.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class localprewith_swap
+{
+public static void main(String[] xy){
+int a=54;
+int b=78;
+System.out.println("Before swaping a= "+a +" and b = "+b);
+int temp=a;
+a=b;
+b=temp;
+System.out.println("After swaping the value of a is :"+a + "and the value of b is :"+b);
+  }
+}

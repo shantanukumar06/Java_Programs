47bef32b76321fcc4f0fd3b8a29b1ec1b0b29876
Shantanu Kumar <128491812+shantanukumar06@users.noreply.github.com>
2023-04-14 00:16:15 +0530

    Add files via upload

--- union.java
+++ union.java
@@ -47,6 +47,32 @@ for(int l=0;l<k;l++){
 System.out.print(result[l] + " ");
 }
 }
+static void Intersection(int arr[],int brr[],int n,int m){
+int i=0,j=0,k=0,l=0;
+int []result=new int[n+m];
+while(i<n&&j<m){
+if(arr[i]<brr[j]){
+i++;
+}else if(arr[i]>brr[j]){
+j++;
+}
+else{
+if(k!=0&&arr[i]==result[k-1]){
+i++;
+j++;
+}else{
+result[k]=arr[i];
+i++;
+j++;
+k++;
+}
+}
+}
+System.out.print("Intersection of two array : ");
+for( l=0;l<k;l++){
+System.out.print(result[l] + " ");
+}
+}
 public static void main(String args[]){
 int arr[],brr[];
 arr = new int[5];
@@ -65,7 +91,8 @@ Arrays.sort(arr);
 Arrays.sort(brr);
 int n= arr.length;
 int m = brr.length;
-Unions(arr,brr,n,m);
+  Unions(arr,brr,n,m);
+Intersection(arr,brr,n,m);
 }
 
 }
